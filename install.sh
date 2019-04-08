a48c17f7555abcc038469e5c99505d67aa3969c8
Kelemen Szimonisz <kelemen.szimonisz@gmail.com>
2019-04-08 02:17:49 -0700

    adding install script

---
+++ install.sh
@@ -0,0 +1,44 @@
+echo "Installing smartkinect"
+echo "Discovering or installing required dependencies via Homebrew for Mac OS X..."
+
+brew update
+brew install libusb
+brew tap brewsci/science
+brew tap totakke/openni2
+brew install openni2
+brew install opencv@2
+
+echo "Copying openni2_libfreenect2_drivers into openni2's system Drivers folder"
+cp -r ./openni2_libfreenect2_drivers/. /usr/local/opt/openni2/lib/ni2/Drivers
+
+echo "Downloading NiTE 2.2 (almost 200MB will take a second)"
+curl -LO https://bitbucket.org/kaorun55/openni-2.2/raw/2f54272802bfd24ca32f03327fbabaf85ac4a5c4/NITE%202.2%20α/NiTE-MacOSX-x64-2.2.tar.zip
+
+echo "Unarchiving NiTE 2.2"
+tar xvf NiTE-MacOSX-x64-2.2.tar.zip
+tar xvf NiTE-MacOSX-x64-2.2.tar.bz2
+mv ./NiTE-MacOSX-x64-2.2 ./Nite2.2
+rm NiTE-MacOSX-x64-2.2.tar.bz2
+rm NiTE-MacOSX-x64-2.2.tar.zip
+
+echo "install_name_tool helper for libNiTE2.dylib->libOpenNI2.dylib"
+install_name_tool -change libOpenNI2.dylib /usr/local/opt/openni2/lib/ni2/libOpenNI2.dylib ./Nite2.2/Redist/libNiTE2.dylib
+
+echo "Copying OpenNI2 header files to Nite2.2/Include folder"
+cp /usr/local/opt/openni2/include/ni2/{OniCAPI.h,OniPlatform.h,OniCTypes.h,OpenNI.h} ./Nite2.2/Include/
+
+echo "Creating a build directory"
+mkdir build
+cd build
+
+echo "Building and compiling KinectFeed"
+cmake ..
+make
+
+echo "install_name_tool helper for KinectFeed->libNiTE2.dylib"
+install_name_tool -change libNiTE2.dylib @executable_path/../Nite2.2/Redist/libNiTE2.dylib KinectFeed
+
+echo "Moving NiTE2.2 Data folder to executable's root directory"
+cp -r ../Nite2.2/Redist/NiTE2 ./
+
+echo "Installation is Complete. ./build/KinectFeed to run"
